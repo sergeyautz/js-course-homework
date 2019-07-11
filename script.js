c65bd9ab89f80edc0087dde5115dbdcbe9bae7ec
Sergey Auts <sergeyauts@gmail.com>
2019-07-11 15:33:24 +0300

    исправил домашку по образцу Яна

--- script.js
+++ script.js
@@ -2,44 +2,31 @@
 let money;
 let time;
 
-money = prompt ("Ваш бюджет на месяц?");
-time = prompt ("Введите дату в формате YYYY-MM-DD");
+money = prompt("Ваш бюджет на месяц?");
+time = prompt("Введите дату в формате YYYY-MM-DD");
 
 // 3
 
 let appData = {
     budget: money,
     timeData: time,
-    expenses:  {
-
-
-
-    },
-
-    optionalExpenses:  {
-
-
-    },
-
-    income: [
-
-    ],
-
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
     savings: false
-
-
-}
+  
+};
 
 // 4
 
-let expMustHaveName = prompt ('Введите обязательную статью расходов в этом месяце');
-let expMustHaveAmount = prompt ('Во сколько обойдется?');
+let expMustHaveName = prompt('Введите обязательную статью расходов в этом месяце');
+let expMustHaveAmount = prompt('Во сколько обойдется?');
 
 appData.expenses.expMustHaveName = expMustHaveAmount;
 
 // 5
 
-alert ('Ваш бюджет на месяц: ' + appData.budget/30);
+alert('Ваш бюджет на месяц: ' + appData.budget / 30);
 console.log('Hello World!');
 
 
